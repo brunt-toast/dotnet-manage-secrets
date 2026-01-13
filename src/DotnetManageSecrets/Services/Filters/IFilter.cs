76b380d46a15bb4a02607f444e31d6fca1c27523
Josh Brunton <josh.brunton@proton.me>
2026-01-13 15:44:04 +0000

    Fresh start (v2 prep)

--- src/DotnetManageSecrets/Services/Filters/IFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dev.JoshBrunton.DotnetManageSecrets.Types;
-
-namespace Dev.JoshBrunton.DotnetManageSecrets.Services.Filters;
-
-public interface IFilter
-{
-    Result<string> Clean(string input);
-    Result<string> Smudge(string input);
-}
